f72dc46c11c2afb9d7d40bda97711e16562cd5e1
FrameHehe <13939298276@163.com>
2024-04-06 03:58:57 +0800

    update

--- exercises/tests/build.rs
+++ exercises/tests/build.rs
@@ -11,15 +11,15 @@ fn main() {
         .unwrap()
         .as_secs(); // What's the use of this timestamp here?
  
-    // let your_command = format!(
-    //     "rustc-env=TEST_FOO={}",
-    //     timestamp
-    // );
-    // println!("cargo:{}", your_command);
+    let your_command = format!(
+        "rustc-env=TEST_FOO={}",
+        timestamp
+    );
+    println!("cargo:{}", your_command);
 
     // In tests8, we should enable "pass" feature to make the
     // testcase return early. Fill in the command to tell
     // Cargo about that.
-    let your_command = format!("rustc-cfg=feature=\"pass\" rustc-env=TEST_FOO={}",timestamp);
+    let your_command = "rustc-cfg=feature=\"pass\" ";
     println!("cargo:{}", your_command);
 }
